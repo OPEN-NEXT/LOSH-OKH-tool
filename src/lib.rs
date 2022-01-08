c04bd52b229baec872bceaa7dcfcb73688100319
hoijui <hoijui.quaero@gmail.com>
2022-01-08 18:52:55 +0100

    cargo: disable unused code warnings

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,7 @@
 
 //#![warn(clippy::all)]
 //#![warn(rust_2018_idioms)]
+#![allow(dead_code)]
 
 extern crate jsonschema;
 extern crate log;
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@
 
 #![warn(clippy::all)]
 #![warn(rust_2018_idioms)]
+#![allow(dead_code)]
 
 mod cli;
 mod conversion;
