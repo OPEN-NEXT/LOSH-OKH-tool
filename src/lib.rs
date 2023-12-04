c09cd343dd66f080be1dc6b6369fcf909223c533
Robin Vobruba <hoijui.quaero@gmail.com>
2023-12-04 16:42:36 +0100

    Doc-test rust code in the README

--- src/lib.rs
+++ src/lib.rs
@@ -12,4 +12,10 @@ mod validation;
 
 use git_version::git_version;
 
+// This tests rust code in the README with doc-tests.
+// Though, It will not appear in the generated documentaton.
+#[doc = include_str!("../README.md")]
+#[cfg(doctest)]
+pub struct ReadmeDoctests;
+
 pub const VERSION: &str = git_version!(cargo_prefix = "", fallback = "unknown");
