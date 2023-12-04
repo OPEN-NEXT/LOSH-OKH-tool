4596758fabe01dd31da43265ddb3ecc845ad4a46
Robin Vobruba <hoijui.quaero@gmail.com>
2023-12-04 16:42:09 +0100

    Removes deprecated `extern crate` instructions [minor]

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,6 @@
-// SPDX-FileCopyrightText: 2021 Robin Vobruba <hoijui.quaero@gmail.com>
+// SPDX-FileCopyrightText: 2021 - 2023 Robin Vobruba <hoijui.quaero@gmail.com>
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
-
-extern crate jsonschema;
-extern crate log;
-extern crate num_derive;
-extern crate num_traits;
-extern crate once_cell;
-extern crate regex;
-extern crate serde;
-extern crate serde_json;
-extern crate serde_yaml;
-extern crate spdx;
-extern crate strum;
-extern crate strum_macros;
-extern crate thiserror;
-extern crate toml;
 
 mod conversion;
 mod formats;
